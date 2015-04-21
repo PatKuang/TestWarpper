04ad4620b28e4cae23dbe4698ac7af0e1b6cd384
Patrick (Pei) Kuang <patrickuang@gmail.com>
2015-04-21 21:20:21 +0800

    Initial Commit

---
+++ TestMixC++/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestMixC++
+//
+//  Created by Patrick (Pei) Kuang on 4/21/15.
+//  Copyright (c) 2015 FutureGeeks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestMixC++/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TestMixC++
+//
+//  Created by Patrick (Pei) Kuang on 4/21/15.
+//  Copyright (c) 2015 FutureGeeks. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
